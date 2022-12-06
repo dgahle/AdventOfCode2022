20de25c5ebd1e77b1946d38817c308736a125b8f
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-06 14:26:57 +0000

    Added functionality and tests to answer part 2.

--- python/scripts/day_2.py
+++ python/scripts/day_2.py
@@ -1,6 +1,6 @@
 # Imports
 from numpy import array, ndarray
-from pandas import concat, DataFrame, read_csv, read_excel
+from pandas import concat, DataFrame, merge, read_csv, read_excel
 from pathlib import Path
 
 
@@ -102,7 +102,7 @@ def get_match_points(df: DataFrame) -> int:
 
     Win = 6 points
     Lose = 0 points
-    Draw = 0 points
+    Draw = 3 points
 
     :param (DataFrame) df: Long DataFrame of each round of the game in _key_ format
     :return (int) match_points: Points awarded to the Antagonist for winning, losing, or drawing
@@ -157,10 +157,10 @@ def test_rock_paper_scissors_by_key() -> None:
     # Test
     expected_score: int = 15
     assert_msg: str = f"score ({score}) != expected_score ({expected_score})!"
-    assert score==expected_score, assert_msg
+    assert score == expected_score, assert_msg
 
 
-def main() -> None:
+def part_1() -> None:
     """
     Task: What would your total score be if everything goes exactly according to your strategy guide?
 
@@ -175,15 +175,150 @@ def main() -> None:
 
     :return None:
     """
-    # Test
-    test_rock_paper_scissors_by_key()
     # Load
     columns: list[str] = ['Opponent', 'Antagonist']
     df: DataFrame = read_csv(INPUT_PATH, sep=' ', names=columns)
     # Calculate the score
     score: int = rock_paper_scissors_by_key(df)
-    msg: str = f"Total score for the antagonist is {score} points!"
+    msg: str = f"Part 1) Total score for the antagonist is {score} points!"
+    print(msg, end='\n\n')
+
+
+def get_part_2_choice_points(df: DataFrame) -> int:
+    """
+    Order:
+        - Keys -> Choice
+        - Choice -> Outcome
+        - Outcome -> Antagonist Choice
+        - Antagonist Choice -> Choice Points
+    :param df:
+    :return:
+    """
+    # Step 1
+    df_key_outcome: DataFrame = df_rules['key_outcome'].copy().rename(columns=dict(Key='Antagonist'))
+    columns: list[str] = ['Opponent', 'Outcome']
+    df = df.merge(df_key_outcome)  # [columns]
+    df = df.merge(df_rules['outcome_points'])
+    # Step 2
+    df_mapping: DataFrame = df_match_points_mapping.copy()
+    i_col: int
+    col: str
+    columns: list[str] = ['Opponent', 'Antagonist']
+    frames: list[DataFrame] = []
+    for i_col, col in enumerate(columns):
+        _df: DataFrame = df_mapping['Match'].apply(lambda x: x[i_col]).to_frame(name=col)
+        frames.append(_df)
+    df_mapping = concat([*frames, df_mapping], axis=1)
+    df_mapping = df_mapping.merge(df_rules['outcome_points'])
+    col: str = 'Opponent-Outcome'
+    frames = [
+        df_mapping,
+        df_mapping[['Opponent', 'Outcome']].sum(1).to_frame(col)
+    ]
+    df_mapping = concat(frames, axis=1)
+    # Step 3
+    df = df[['Opponent', 'Outcome']].sum(1).to_frame(col)
+    df = df.merge(df_mapping[[col, 'Antagonist']])
+    df = df.rename(columns=dict(Antagonist='Key'))
+    df = df.merge(df_rules['key_choice'][['Key', 'Choice']])
+    df = df.merge(df_rules['choice_points'])
+    # Step 4
+    choice_points: int = df['Points'].sum()
+    return choice_points
+
+
+def get_part_2_match_score(df: DataFrame) -> int:
+    """
+
+    :param df:
+    :return:
+    """
+    new_columns: str = 'Antagonist'
+    df_antagonist_key_points: DataFrame = merge(df_rules['key_outcome'], df_rules['outcome_points'], on='Outcome',
+                                                how='inner')
+    df_antagonist_key_points = df_antagonist_key_points.rename(columns=dict(Key=new_columns))
+    df_match_points: DataFrame = df[[new_columns]].merge(df_antagonist_key_points, on=new_columns, how='inner')
+    match_points: int = df_match_points['Points'].sum()
+    return match_points
+
+
+def test_part_2() -> None:
+    """
+
+    :return:
+    """
+    # Load data
+    columns: list[str] = ['Opponent', 'Antagonist']
+    df: DataFrame = read_csv(TEST_INPUT_PATH, sep=' ', names=columns)
+    # Choice points
+    choice_points: int = get_part_2_choice_points(df)
+    # Match points
+    match_points: int = get_part_2_match_score(df)
+    # Calculate the score
+    score: int = choice_points + match_points
+    # Test
+    expected_score: int = 12
+    assert_msg: str = f"score ({score}) != expected_score ({expected_score})!"
+    assert score == expected_score, assert_msg
+
+
+def part_2() -> None:
+    """
+    The Elf finishes helping with the tent and sneaks back over to you:
+        "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end
+         the round in a draw, and Z means you need to win. Good luck!"
+
+    The total score is still calculated in the same way, but now you need to figure out what shape to choose so the
+    round ends as indicated.
+
+    The example above now goes like this:
+
+        In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you
+        also choose Rock.
+        This gives you a score of 1 + 3 = 4.
+        In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of
+        1 + 0 = 1.
+        In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+        Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+    Following the Elf's instructions for the second column, what would your total score be if everything goes exactly
+    according to your strategy guide?
+
+    :return:
+    """
+    # Load
+    columns: list[str] = ['Opponent', 'Antagonist']
+    df: DataFrame = read_csv(INPUT_PATH, sep=' ', names=columns)
+    # Choice points
+    choice_points: int = get_part_2_choice_points(df)
+    # Match points
+    match_points: int = get_part_2_match_score(df)
+    # Calculate the score
+    score: int = choice_points + match_points
+    msg: str = f"Part 2) Total score for the antagonist is {score} points!"
     print(msg)
+
+
+def tests() -> None:
+    """
+
+    :return:
+    """
+    test_rock_paper_scissors_by_key()
+    test_part_2()
+
+
+def main() -> None:
+    """
+
+    :return None:
+    """
+    # Test functions
+    tests()
+    # Part 1
+    part_1()
+    # Part 2
+    part_2()
     pass
 
 
