da65a5df48537a793e71579ff33247d651072c24
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-06 18:05:03 +0000

    Base templates for main, functions, and tests.

---
+++ python/scripts/day_5/main.py
@@ -0,0 +1,16 @@
+# Imports
+from pathlib import Path
+
+
+# Variables
+REPO_PATH: Path = Path(__file__).parent.parent.parent.parent
+INPUT_PATH: Path = REPO_PATH / 'input' / 'day_5' / 'input.txt'
+
+
+# Functions
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
