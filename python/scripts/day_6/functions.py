cea65df5d692e50c5dfee2137c18d286a1420973
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-07 21:21:02 +0000

    Made the unique length a keyword in the decode function.

--- python/scripts/day_6/functions.py
+++ python/scripts/day_6/functions.py
@@ -9,16 +9,16 @@ PYTHON_SCRIPTS_DIR_PATH: Path = REPO_PATH / 'python' / 'scripts'
 
 
 # Functions
-def get_index_of_first_packet(data: str) -> int:
+def get_index_of_first_packet(data: str, unique_len: int) -> int:
     """
     In the protocol being used by the Elves, the start of a packet is indicated by a sequence of four characters that
     are all different.
 
     :param (str) data:
+    :param (int) unique_len:
     :return (int) index:
     """
     index: int
-    unique_len: int = 4
     scan_len: int = len(data) - unique_len
     for index in range(scan_len):
         slice_stop: int = index + unique_len
--- python/scripts/day_6/tests.py
+++ python/scripts/day_6/tests.py
@@ -8,6 +8,7 @@ from python.scripts.day_6.functions import get_index_of_first_packet
 REPO_PATH: Path = Path(__file__).parent.parent.parent.parent
 INPUT_DIR_PATH: Path = REPO_PATH / 'input' / 'day_6'
 TEST_DATA_PATH: Path = INPUT_DIR_PATH / 'test_data.csv'
+TEST_DATA_PART_2_PATH: Path = INPUT_DIR_PATH / 'test_data_part_2.csv'
 PYTHON_SCRIPTS_DIR_PATH: Path = REPO_PATH / 'python' / 'scripts'
 
 
@@ -21,7 +22,30 @@ def test_get_index_of_first_packet() -> None:
     df: DataFrame = read_csv(TEST_DATA_PATH)
     # Run function
     name: str = 'trial'
-    df_output: DataFrame = df['input'].apply(get_index_of_first_packet).to_frame(name)
+    df_output: DataFrame = df['input'].apply(
+        get_index_of_first_packet,
+        args=[4]
+    ).to_frame(name)
+    # Group
+    frames: list[DataFrame] = [df, df_output]
+    df = concat(frames, axis=1)
+    # Assert check
+    assert df['output'].equals(df['trial'])
+
+
+def test2_get_index_of_first_packet() -> None:
+    """
+
+    :return:
+    """
+    # Load test data
+    df: DataFrame = read_csv(TEST_DATA_PART_2_PATH)
+    # Run function
+    name: str = 'trial'
+    df_output: DataFrame = df['input'].apply(
+        get_index_of_first_packet,
+        args=[14]
+    ).to_frame(name)
     # Group
     frames: list[DataFrame] = [df, df_output]
     df = concat(frames, axis=1)
@@ -31,6 +55,7 @@ def test_get_index_of_first_packet() -> None:
 
 def main() -> None:
     test_get_index_of_first_packet()
+    test2_get_index_of_first_packet()
     pass
 
 
