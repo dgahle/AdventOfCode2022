e055798a087d3dd2eb25b00a28929ca81209c9d0
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-07 21:24:47 +0000

    Updated to calculate asnwer to part 1 and 2

--- python/scripts/day_6/main.py
+++ python/scripts/day_6/main.py
@@ -24,10 +24,15 @@ def main() -> None:
     with open(INPUT_DATA_PATH, 'r') as f:
         data: str = f.read()
     # Find index
-    index: int = get_index_of_first_packet(data)
-    print('How many characters need to be processed before the first start-of-packet marker is detected.')
-    print(f"{' '*4} = {index} characters")
-    pass
+    part: int
+    scanning_len: int
+    scanning_lengths: list[int] = [4, 14]
+    for part, scanning_len in enumerate(scanning_lengths):
+        part += 1
+        index: int = get_index_of_first_packet(data, unique_len=scanning_len)
+        task_msg: str = 'How many characters need to be processed before the first start-of-packet marker is detected.'
+        print(f'Part {part}: {task_msg}')
+        print(f"{' '*4} = {index} characters (with unique length of {scanning_len})")
 
 
 if __name__ == "__main__":
