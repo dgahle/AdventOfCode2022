54e6a4292dab72c7969968726d14f6ba8236e8a5
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-07 20:45:15 +0000

    Updated python tempalate to include repo path

--- python/template.py
+++ python/template.py
@@ -1,7 +1,11 @@
 # Imports
+from pathlib import Path
 
 
 # Variables
+REPO_PATH: Path = Path(__file__).parent.parent.parent.parent
+INPUT_DIR_PATH: Path = REPO_PATH / 'input'
+PYTHON_SCRIPTS_DIR_PATH: Path = REPO_PATH / 'python' / 'scripts'
 
 
 # Functions
