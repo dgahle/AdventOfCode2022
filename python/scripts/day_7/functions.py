83dc7a2144aa23fad9b5a4d76cd3b8f47635d66e
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-07 21:29:44 +0000

    Templates for python scripts.

---
+++ python/scripts/day_7/functions.py
@@ -0,0 +1,17 @@
+# Imports
+from pathlib import Path
+
+
+# Variables
+REPO_PATH: Path = Path(__file__).parent.parent.parent.parent
+INPUT_DIR_PATH: Path = REPO_PATH / 'input'
+PYTHON_SCRIPTS_DIR_PATH: Path = REPO_PATH / 'python' / 'scripts'
+
+
+# Functions
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
