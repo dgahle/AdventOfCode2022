1784d9da2fdd06f39bdbc598e92e8f31cb2d66a3
Daljeet Gahle <daljeet.gahle@ukhsa.gov.uk>
2022-12-07 18:17:45 +0000

    Writted functions and tests for taking step(s) and reading the top of the puzzle.

--- python/scripts/day_5/functions.py
+++ python/scripts/day_5/functions.py
@@ -1,5 +1,8 @@
 # Imports
+from numpy import arange, array, concatenate, ndarray, zeros
 from pathlib import Path
+from re import match
+from typing import Optional
 
 
 # Variables
@@ -8,6 +11,132 @@ INPUT_PATH: Path = REPO_PATH / 'input' / 'day_5' / 'input.txt'
 
 
 # Functions
+def row_to_row_of_cells(row: str, num_cells: int) -> list[str]:
+    row_out: list[str] = []
+    n_cell: int
+    len_cell: int = 3
+    lhs_index: int = 0
+    for n_cell in range(num_cells):
+        rhs_index: int = lhs_index + len_cell
+        cell_slice: slice = slice(lhs_index, rhs_index)
+        cell: str = row[cell_slice]
+        row_out.append(cell)
+        lhs_index += 1 + len_cell
+
+    return row_out
+
+
+def preprocess_puzzle(data: str) -> ndarray:
+    # raw data to lines
+    lines: list[str] = data.split('\n')
+    # process rows to columns
+    num: str
+    columns_nums: list[int] = [int(num) for num in lines[-1].split('  ')]
+    num_columns: int = max(columns_nums)
+    # len row = num_columns * 3 + (num_columns - 1)
+    #
+    row: str
+    lines: list[list[str]] = [row_to_row_of_cells(row, num_cells=num_columns) for row in lines[:-1]]
+    # rows to columns
+    matrix: ndarray = array(lines)
+    # Standardise empty cells
+    i: int
+    empty_cell: str = ''
+    for i in range(3):
+        i += 1
+        empty_space: str = ' '*i
+        check: ndarray = (empty_space == matrix)
+        matrix[check] = empty_cell
+
+    return matrix
+
+
+def preprocess_inputs(data: str) -> tuple[ndarray, list[str]]:
+    # Separate data and instructions
+    data: str
+    instructions: str
+    separator: str = '\n\n'
+    data, instructions = data.split(sep=separator)
+    # process data
+    data: ndarray = preprocess_puzzle(data)
+    # process instructions
+    separator: str = '\n'
+    instructions: list[str] = instructions.split(sep=separator)
+    # Return
+    return data, instructions
+
+
+def read_the_top(data: ndarray) -> str:
+    """
+
+    :param data:
+    :return:
+    """
+    n_rows: int
+    n_cols: int
+    n_rows, n_cols = data.shape
+    top_row: list[str] = []
+    i_col: int
+    cell: str
+    for i_col in range(n_cols):
+        row: ndarray = data[:, i_col]
+        for cell in row:
+            if match(r'\[\w]', cell):
+                top_row.append(cell[1:-1])
+                break
+    # Sum
+    top_row: str = ''.join(top_row)
+    return top_row
+
+
+def take_step(data: ndarray, instruction: str) -> ndarray:
+    # Get parameters of data
+    num_rows: int
+    num_cols: int
+    num_rows, num_cols = data.shape
+    # Format string instruction to int
+    instruction: ndarray = array(instruction.split(' '))[1 + 2 * arange(3)]
+    num_move, col_from, col_to = instruction.astype(int).tolist()
+    # Index correction (starts at 1 -> starts at 0)
+    index_correction: int = -1
+    col_from += index_correction
+    col_to += index_correction
+    # Get cell(s) to move
+    empty_cell: str = ''
+    cell_lhs_index: int = max([0, (empty_cell == data[:, col_from]).sum()])
+    cell_rhs_index: int = cell_lhs_index + num_move
+    cell_index: slice = slice(cell_lhs_index, cell_rhs_index)
+    cell_from_index: tuple[slice, int] = (cell_index, col_from)
+    cells: ndarray = data[cell_from_index].copy()
+    data[cell_from_index] = empty_cell
+    # Add cell to new place
+    cell_lhs_index: int = max([0, (empty_cell == data[:, col_to]).sum() - 1])
+    cell_rhs_index: int = cell_lhs_index + num_move
+    cell_index: slice = slice(cell_lhs_index, cell_rhs_index)
+    cell_to_index: tuple[slice, int] = (cell_index, col_to)
+    # Check if data needs to be expanded
+    num_rows_to_add: int = cell_index.stop - num_rows
+    if num_rows_to_add > 0:
+        # Build missing rows
+        shape: tuple[int, int] = (num_rows_to_add, num_cols)
+        extra_rows: ndarray = zeros(shape, dtype=str)
+        # Add addition rows
+        arrays: tuple[ndarray, ndarray] = (extra_rows, data)
+        data = concatenate(arrays, axis=0)
+        # Update date
+        cell_rhs_index: int = max([0, (empty_cell == data[:, col_to]).sum()])
+        cell_lhs_index: int = cell_rhs_index - num_move
+        cell_index: slice = slice(cell_lhs_index, cell_rhs_index)
+        cell_to_index: tuple[slice, int] = (cell_index, col_to)
+        data[cell_to_index] = cells[::-1]
+    else:
+        data[cell_to_index] = cells
+    # Check for empty rows
+    zero_row_check: Optional[ndarray, bool] = data.shape[1] == (zeros(data.shape[1], dtype=str) == data).sum(1)
+    data = data[~zero_row_check]
+    return data
+
+
 def main() -> None:
     pass
 
--- python/scripts/day_5/tests.py
+++ python/scripts/day_5/tests.py
@@ -1,14 +1,158 @@
 # Imports
+from numpy import ndarray
 from pathlib import Path
+from python.scripts.day_5.functions import preprocess_inputs, preprocess_puzzle, read_the_top, take_step
 
 
 # Variables
 REPO_PATH: Path = Path(__file__).parent.parent.parent.parent
-INPUT_PATH: Path = REPO_PATH / 'input' / 'day_5' / 'input.txt'
+INPUT_DIR_PATH: Path = REPO_PATH / 'input' / 'day_5'
+TEST_INPUT_PATH: Path = INPUT_DIR_PATH / 'test_input.txt'
+TEST_OUTPUT_PATH: Path = INPUT_DIR_PATH / 'test_output.txt'
+TEST_OUTPUT_STEP_1_PATH: Path = INPUT_DIR_PATH / 'test_output_step_1.txt'
+TEST_OUTPUT_STEP_4_PATH: Path = INPUT_DIR_PATH / 'test_output_step_4.txt'
 
 
 # Functions
+def test_read_the_top() -> None:
+    # Load data
+    # Input
+    with open(TEST_OUTPUT_STEP_4_PATH, 'r') as f:
+        data: str = f.read()
+    # Expectation
+    with open(TEST_OUTPUT_PATH, 'r') as f:
+        expectation: str = f.read()
+    # Run function
+    data: ndarray = preprocess_puzzle(data)
+    output: str = read_the_top(data)
+    # Compare against expectation
+    assert_msg: str = f"expectation ({expectation}) != output ({output})!"
+    assert expectation == output, assert_msg
+
+
+def test_take_step() -> None:
+    # Load data
+    with open(TEST_INPUT_PATH, 'r') as f:
+        data_raw: str = f.read()
+    # Preprocess inputs
+    data: ndarray
+    instructions: list[str]
+    data, instructions = preprocess_inputs(data_raw)
+    # Take step
+    num_test: int
+    result: ndarray
+    step: int
+    num_test: int = 1
+    for step in range(num_test):
+        instruction: str = instructions[step]
+        if step == 0:
+            result = take_step(data.copy(), instruction)
+        else:
+            take_step(result, instruction)
+    # Compare against expectation
+    # Load
+    test_output_path: Path = INPUT_DIR_PATH / f'test_output_step_{num_test}.txt'
+    with open(test_output_path, 'r') as f:
+        expectation_raw: str = f.read()
+        expectation: ndarray = preprocess_puzzle(expectation_raw)
+    # Compare
+    assert_check: ndarray = (expectation == result)
+    assert_msg: str = f"Test {num_test}) Expectation ({expectation}) != output ({result})!"
+    assert assert_check.all(), assert_msg
+
+
+def test_take_2_steps() -> None:
+    # Load data
+    with open(TEST_INPUT_PATH, 'r') as f:
+        data_raw: str = f.read()
+    # Preprocess inputs
+    data: ndarray
+    instructions: list[str]
+    data, instructions = preprocess_inputs(data_raw)
+    # Take step
+    num_test: int
+    result: ndarray
+    step: int
+    num_test: int = 2
+    for step in range(num_test):
+        instruction: str = instructions[step]
+        result = take_step(data.copy(), instruction) if step == 0 else take_step(result.copy(), instruction)
+    # Compare against expectation
+    # Load
+    test_output_path: Path = INPUT_DIR_PATH / f'test_output_step_{num_test}.txt'
+    with open(test_output_path, 'r') as f:
+        expectation_raw: str = f.read()
+        expectation: ndarray = preprocess_puzzle(expectation_raw)
+    # Compare
+    assert_check: ndarray = (expectation == result)
+    assert_msg: str = f"Test {num_test}) Expectation ({expectation}) != output ({result})!"
+    assert assert_check.all(), assert_msg
+
+
+def test_take_3_steps() -> None:
+    # Load data
+    with open(TEST_INPUT_PATH, 'r') as f:
+        data_raw: str = f.read()
+    # Preprocess inputs
+    data: ndarray
+    instructions: list[str]
+    data, instructions = preprocess_inputs(data_raw)
+    # Take step
+    num_test: int
+    result: ndarray
+    step: int
+    num_test: int = 3
+    for step in range(num_test):
+        instruction: str = instructions[step]
+        result = take_step(data.copy(), instruction) if step == 0 else take_step(result.copy(), instruction)
+    # Compare against expectation
+    # Load
+    test_output_path: Path = INPUT_DIR_PATH / f'test_output_step_{num_test}.txt'
+    with open(test_output_path, 'r') as f:
+        expectation_raw: str = f.read()
+        expectation: ndarray = preprocess_puzzle(expectation_raw)
+    # Compare
+    assert_check: ndarray = (expectation == result)
+    assert_msg: str = f"Test {num_test}) Expectation ({expectation}) != output ({result})!"
+    assert assert_check.all(), assert_msg
+
+
+def test_take_4_steps() -> None:
+    # Load data
+    with open(TEST_INPUT_PATH, 'r') as f:
+        data_raw: str = f.read()
+    # Preprocess inputs
+    data: ndarray
+    instructions: list[str]
+    data, instructions = preprocess_inputs(data_raw)
+    # Take step
+    num_test: int
+    result: ndarray
+    step: int
+    num_test: int = 3
+    for step in range(num_test):
+        instruction: str = instructions[step]
+        result = take_step(data.copy(), instruction) if step == 0 else take_step(result.copy(), instruction)
+    # Compare against expectation
+    # Load
+    test_output_path: Path = INPUT_DIR_PATH / f'test_output_step_{num_test}.txt'
+    with open(test_output_path, 'r') as f:
+        expectation_raw: str = f.read()
+        expectation: ndarray = preprocess_puzzle(expectation_raw)
+    # Compare
+    assert_check: ndarray = (expectation == result)
+    assert_msg: str = f"Test {num_test}) Expectation ({expectation}) != output ({result})!"
+    assert assert_check.all(), assert_msg
+
+
 def main() -> None:
+    # Read the top
+    test_read_the_top()
+    # Take steps
+    test_take_step()
+    test_take_2_steps()
+    test_take_3_steps()
+    test_take_4_steps()
     pass
 
 
